6b0cd2b12decfeaeee86e54266bb91a6c57312ee
RubDev666 <125171500+RubDev666@users.noreply.github.com>
2024-04-03 22:09:34 -0600

    solucion-deployment

--- app/orders/api/route.tsx
+++ app/orders/api/route.tsx
@@ -1,6 +1,6 @@
 import { prisma } from "@/src/lib/prisma";
 
-export const dynamic = 'force-dynamic'
+export const dynamic = 'force-dynamic'; //para solucionar problema en produccion
 
 export async function GET() {
     const orders = await prisma.order.findMany({
